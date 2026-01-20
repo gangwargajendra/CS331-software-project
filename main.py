22ddcdd2bff94416653f379f2d9169047d193e61
K-K-R157 <kundanrider555666@gmail.com>
2026-01-20 21:53:15 +0530

    opened opening and displaying

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture('./test_videos/traffic1.mp4')
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    cv2.imshow('Traffic Feed', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
